daf272c71d79862405e18036906534d67ee38c16
S.M Asif Rahman <94311953+asif-smar@users.noreply.github.com>
2022-12-28 15:28:31 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+function bulbOn(){
+    let bulb=document.querySelector(".bulboff");
+    bulb.setAttribute("src","bulb_on.png");
+}
+function bulbOff(){
+    let bulb=document.querySelector(".bulboff");
+    bulb.setAttribute("src","bulb_off.png");
+}
